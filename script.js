80d1a8f08f9b7df004e14c59f49b2abf101ef19b
mjd10m <mjd10m@outlook.com>
2022-06-30 23:37:44 -0400

    final

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Assignment code here
 
-//const to define characters allowed for each type
-const LOWERCASECHARACTERS = 'qwertyuioplkjhgfdsazxcvbnm'
-const UPPERCASECHARACTERS = 'QWERTYUIOPLKJHGFDSAZXCVBNM'
-const NUMBERCHARACTERS = '0123456789'
-const SPECIALCHARACTERS = '!#$%&()*+,-./:;<=>?@[^_]`{}\|~'
+//CONST to define characters allowed for each type
+const LOWERCASECHARACTERS = 'qwertyuioplkjhgfdsazxcvbnm';
+const UPPERCASECHARACTERS = 'QWERTYUIOPLKJHGFDSAZXCVBNM';
+const NUMBERCHARACTERS = '0123456789';
+const SPECIALCHARACTERS = '!#$%&()*+,-./:;<=>?@[^_]`{}\|~';
 
-// function which generates password when Generate Button is clicked on webpage
+//Function which generates password when Generate Button is clicked on webpage
 function generatePassword(passwordLength) {
   
-  var passwordLengthValidated = passwordLength 
+  var passwordLengthValidated = passwordLength; 
   if (passwordLengthValidated === undefined) {
     passwordLengthValidated = validatePasswordLen();
   }
@@ -20,10 +20,6 @@ function generatePassword(passwordLength) {
     Number: window.confirm("Include numbers? Ok for Yes Cancel for No."),
     Special: window.confirm("Include special characters? Ok for Yes Cancel for No.")
   };
-//  var charTypeLowercase = window.confirm("Include lower case characters? Ok for Yes Cancel for No.");
-//  var charTypeUppercase = window.confirm("Include upper case characters? Ok for Yes Cancel for No.");
-//  var charTypeNumber = window.confirm("Include numbers? Ok for Yes Cancel for No.");
-//  var charTypeSpecial = window.confirm("Include special characters? Ok for Yes Cancel for No.");
   
   //Checks that atleast one character type was selected if not loop back through prompts
   if (charType.Lowercase === false && charType.Uppercase === false && charType.Number === false && charType.Special === false) {
@@ -33,11 +29,11 @@ function generatePassword(passwordLength) {
   
   var passwordUnshuffled = createPassword(passwordLengthValidated, charType.Lowercase, charType.Uppercase, charType.Number, charType.Special);
   
-  var finalPassword = shuffleArray(passwordUnshuffled)
+  var finalPassword = shuffleArray(passwordUnshuffled);
 
-//changes final password from array to string
-  return finalPassword.join('')
-} 
+  //Changes final password from array to string
+  return finalPassword.join('');
+};
 
 //Prompts user to input character length and validates whether input is valid 
 function validatePasswordLen() {
@@ -47,48 +43,48 @@ function validatePasswordLen() {
     window.alert("Please select a number length between 8 and 128 characters");
     return validatePasswordLen();
   }
-  return passwordLength
-}
+  return passwordLength;
+};
 
 //Randomly creates a password array
 function createPassword(passwordLengthValidated, charTypeLowercase, charTypeUppercase, charTypeNumber, charTypeSpecial) {
-  var charTypeUsed = []
+  var charTypeUsed = [];
 
   if (charTypeLowercase === true) {
-    charTypeUsed.push('lower')
+    charTypeUsed.push('lower');
   }
   if (charTypeUppercase === true) {
-    charTypeUsed.push('upper')
+    charTypeUsed.push('upper');
   }
   if (charTypeNumber === true) {
-    charTypeUsed.push('number')
+    charTypeUsed.push('number');
   }
   if (charTypeSpecial === true) {
-    charTypeUsed.push('special')
+    charTypeUsed.push('special');
   }
   
   shuffleArray(charTypeUsed);
 
   var passwordCharArray = []
   for (i = 0; i < passwordLengthValidated; i++) {
-    var limit = charTypeUsed.length
-    var passwordChar = ''
+    var limit = charTypeUsed.length;
+    var passwordChar = '';
 
     if(i < limit) {
-      passwordChar = determinePassChar(charTypeUsed[i])
+      passwordChar = determinePassChar(charTypeUsed[i]);
 
       } else {
-        var randomCharType = charTypeUsed[Math.floor(Math.random() * charTypeUsed.length)]
-        passwordChar = determinePassChar(randomCharType)
+        var randomCharType = charTypeUsed[Math.floor(Math.random() * charTypeUsed.length)];
+        passwordChar = determinePassChar(randomCharType);
       }
-      passwordCharArray.push(passwordChar)
+      passwordCharArray.push(passwordChar);
     } 
-  return passwordCharArray
-}
+  return passwordCharArray;
+};
 
-//randomly selects a character from the string passed to it
+//Randomly selects a character from the string passed to it
 function determinePassChar (charType) {
-  var char = ''
+  var char = '';
   switch(charType) {
     case 'lower':
       char = LOWERCASECHARACTERS.charAt(Math.floor(Math.random() * LOWERCASECHARACTERS.length));
@@ -106,7 +102,7 @@ function determinePassChar (charType) {
   }
 }
 
-//shuffles the values within the array given
+//Shuffles the values within the array given
 function shuffleArray(array) {
   let currentIndex = array.length, randomIndex;
 
@@ -116,13 +112,13 @@ function shuffleArray(array) {
 
     [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
   }
-  return array
+  return array;
 }
 
-// Get references to the #generate element
+//Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -131,5 +127,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+//Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
